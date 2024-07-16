194334aa83480ba47cccb502d3cbbe402121f0f9
bpb2008 <brittanythompson08@gmail.com>
2024-07-16 13:13:11 -0500

    installed tailwind css

---
+++ client/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      letterSpacing: {
+        "-1.68": "-1.68px",
+        2: "2px",
+      },
+    },
+  },
+  plugins: [],
+};
